6ccc11ec702bda96a0e3295b9cc85e06d8469b1a
Jody Moore <jodymoore@g.harvard.edu>
2017-02-06 14:06:55 -0600

    init

---
+++ includes/constants.php
@@ -0,0 +1,24 @@
+<?php
+
+    /**
+     * constants.php
+     *
+     * Computer Science 50
+     * Final project
+     *
+     * Global constants.
+     */
+
+    // your database's name
+    define("DATABASE", "jodmoo_rover1");
+
+    // your database's password
+    define("PASSWORD", "groove");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "jody");
+
+?>
