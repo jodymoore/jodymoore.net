a235ff70bb8b5f11c760e4c72f3badaf0992c7da
Jody Moore <jodymoore@g.harvard.edu>
2017-02-06 13:21:42 -0600

    initial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html
+        PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Jody Moore</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+    <link href="/css/styles.css" rel="stylesheet"/>
+
+    <script type="text/javascript" src="js/jquery-1.8.0.min.js"></script> 
+    <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/scripts.js"></script>
+  </head>
+   <div id="top">
+    <h1><font>Welcome to</font></h1> <br>
+    <h1><font>jodymoore.net!</font></h1>
+   </div>
+   <div id="middle"> 
+    <img text-align="center" alt="creds"  src="img/myPic3.jpg"/>
+    
+   </div> 
+   <div id="bottom">
+     <a href="/public/index.php"><h3><font>DevOpsPage</font></h3></a> 
+     <img alt="creds"  src="img/CSA.jpg" width="10%" height="10%" />   
+   </div>
+  </body>
+</html>
