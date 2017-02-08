66acbfdcc9c5eb09540f87dda73e006e08c03537
Jody Moore <jodymoore@g.harvard.edu>
2017-02-08 17:26:33 -0600

    init

---
+++ start.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+  <body>
+   <div id="top">
+    <h1><font>Welcome to</font></h1> 
+    <h1><font>DevOpsPage</font></h1>
+   </div>
+   <div id="middle"> 
+    <img text-align="center" alt="creds"  src="img/myPic3.jpg"/>
+   <nav class="nav nav-pills">
+     <ul>
+       <a href="/public/login.php"><h3><font>Rover1</font></h3></a>
+       <a href="/SimpleInvoice/index.php"><h3><font>SimpleInvoice</font></h3></a>
+     </ul>
+   </nav>
+    
+    
+   </div> 
+   <div id="bottom">
+     <img alt="creds"  src="img/CSA.jpg" width="10%" height="10%" />   
+   </div>
+  </body>
+</html>
