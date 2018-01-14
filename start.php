c0eaca491e66ee64e889414bf073e234798ca3e7
Jody Moore <jodymoore@g.harvard.edu>
2018-01-14 10:39:05 -0600

    new css

--- start.php
+++ start.php
@@ -13,27 +13,39 @@
     <script type="text/javascript" src="public/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="public/js/scripts.js"></script>
 </head>
-  <body>
+<body>
    <div id="top">
    </div>
-   <div id="middle"> 
+  <div id="middle"> 
     <img id="mimg" text-align="center" alt="creds" src="img/dev.png"/>
    <nav class="nav nav-pills">
-     <ul>      
-     <a href="http://a4.jodymoore.net"><img id="iot"  alt="creds"  src="img/pizza-icon.png" width="25px"  height="50px" /></a><br><caption> 
-     <font id ="cap" >Link to Quik Pizza app</font></caption>
+    <ul>      
+     <a href="http://a4.jodymoore.net"><img id="qpa"  alt="creds"  src="img/pizza-icon.png" width="25px"  height="50px" />
+     </a>
      <br>
+     <caption> 
+       <font id ="cap" >Link to Quik Pizza app</font>
+     </caption>
      <br>
-     <ul>      
-     <a href="/public/login.php"><img id="iot"  alt="creds"  src="img/iot.png" width="25px"  height="50px" /></a><br><caption> 
-     <font id ="cap" >Link to Rover1 a network controlled remote operated vehichle</font></caption>
+     <br>     
+     <a href="/public/login.php"><img id="iot"  alt="creds"  src="img/iot.png" width="25px"  height="50px" />
+     </a>
+     <br>
+     <caption> 
+       <font id ="cap" >Link to Rover1 a network controlled remote operated vehichle</font>
+     </caption>
      <br><br>
-     <a href="/SimpleInvoice/index.php"><img id="devpic"  alt="creds"  src="img/devPic.png"/></a><br><caption><font id ="cap" >Link to Simple Invoice current development</font></caption>
-     </ul>
-   </nav>    
-   </div> 
-   <div id="bottom">
+     <a href="/SimpleInvoice/index.php"><img id="devpic"  alt="creds"  src="img/devPic.png"/>
+     </a>
+     <br>
+     <caption>
+       <font id ="cap" >Link to Simple Invoice current development</font>
+     </caption>
+    </ul>
+  </nav>    
+  </div> 
+  <div id="bottom">
      <img alt="creds"  src="img/CSA.jpg" width="10%" height="10%" />   
-   </div>
-  </body>
+  </div>
+</body>
 </html>
